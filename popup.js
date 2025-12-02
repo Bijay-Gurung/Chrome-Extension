07cbc58bdabc0e23c3a5ff30dee7bc4cf622702f
Bijay-Gurung <stha4580@gmail.com>
2025-12-02 16:32:17 +0545

    First chrome extension

--- popup.js
+++ popup.js
@@ -1,1 +1,10 @@
-alert("Hello");
+document.addEventListener('DOMContentLoaded', () => {
+    const displaygif = document.querySelector('.interact');
+    const showBtn = document.getElementById('showBtn');
+
+    if (showBtn && displaygif) {
+        showBtn.addEventListener('click', () => {
+            displaygif.style.display = 'block';
+        });
+    }
+});
